dae7e75c1842c0e1b04a19826432342d2ac9a3be
lipeoliveira <felipecardosodeoliveira02@gmail.com>
2021-08-07 23:47:14 -0300

    modificadores de acesso

--- fundamentos-oop/src/one/digitalinovation/digibank/Pessoa.kt
+++ fundamentos-oop/src/one/digitalinovation/digibank/Pessoa.kt
@@ -3,14 +3,21 @@ package one.digitalinovation.digibank
 class Pessoa {
     var name: String = "Felipe'"
     var age: Int = 28
+    var cpf: String  = "111.111.111.-11"
+
+    private set
 
     inner class Endereco {
         var rua: String = "Rua teste"
     }
+
+
 }
 
 fun main() {
     val p =  Pessoa()
     println(p.name)
     println(p.Endereco().rua)
+
+    println(p.cpf)
 }
