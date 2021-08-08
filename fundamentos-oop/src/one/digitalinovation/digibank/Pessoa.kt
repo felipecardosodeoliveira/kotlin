ee7b6adfdf9208401a83369ba05b10ccc3a477e8
lipeoliveira <felipecardosodeoliveira02@gmail.com>
2021-08-07 23:04:03 -0300

    iniciando com orientação a objetos

--- fundamentos-oop/src/one/digitalinovation/digibank/Pessoa.kt
+++ fundamentos-oop/src/one/digitalinovation/digibank/Pessoa.kt
@@ -3,9 +3,14 @@ package one.digitalinovation.digibank
 class Pessoa {
     var name: String = "Felipe'"
     var age: Int = 28
+
+    inner class Endereco {
+        var rua: String = "Rua teste"
+    }
 }
 
 fun main() {
     val p =  Pessoa()
     println(p.name)
+    println(p.Endereco().rua)
 }
