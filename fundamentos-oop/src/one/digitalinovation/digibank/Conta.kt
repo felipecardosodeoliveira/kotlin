58630507f757b6ff9817c312803cfc1d13b5f602
lipeoliveira <felipecardosodeoliveira02@gmail.com>
2021-08-08 00:29:14 -0300

    conta class

---
+++ fundamentos-oop/src/one/digitalinovation/digibank/Conta.kt
@@ -0,0 +1,15 @@
+package one.digitalinovation.digibank
+
+import java.math.BigDecimal
+
+class Conta (val agencia: String, val numero: String, val sado: BigDecimal) {
+
+    fun deposito(valor: BigDecimal) {
+
+    }
+
+    fun saque(valor: BigDecimal) {
+
+    }
+
+}
